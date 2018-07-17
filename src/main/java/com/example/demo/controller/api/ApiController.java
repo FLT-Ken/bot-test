226f047566892676c02a861fe51ba32b0bc6d540
Ken Chiu <ken.chiu@fullerton.com.tw>
2018-07-17 18:19:08 +0800

    ++

--- src/main/java/com/example/demo/controller/api/ApiController.java
+++ src/main/java/com/example/demo/controller/api/ApiController.java
@@ -1,11 +1,11 @@
 package com.example.demo.controller.api;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.line_bot.vo.Line;
 import com.google.gson.Gson;
 
 @RestController
@@ -14,8 +14,8 @@ public class ApiController {
   private Gson gson = new Gson();
 
   @PostMapping("/message/{message}")
-  public String message(@PathVariable("message") String message, HttpServletRequest req) {
-    System.out.println("req info: " + gson.toJson(req));
+  public String message(@PathVariable("message") String message, @RequestBody Line line) {
+    System.out.println(gson.toJson(line));
     return message;
   }
 }
---
+++ src/main/java/com/example/demo/model/line_bot/vo/Line.java
@@ -0,0 +1,16 @@
+package com.example.demo.model.line_bot.vo;
+
+import java.util.List;
+
+public class Line {
+
+  private List<Event> events;
+
+  public List<Event> getEvents() {
+    return events;
+  }
+
+  public void setEvents(List<Event> events) {
+    this.events = events;
+  }
+}
