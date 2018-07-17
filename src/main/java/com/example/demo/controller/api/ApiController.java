265f7855928ff9619759850d51133c64cb40aa5c
Ken Chiu <ken.chiu@fullerton.com.tw>
2018-07-17 16:29:25 +0800

    加log

--- src/main/java/com/example/demo/controller/api/ApiController.java
+++ src/main/java/com/example/demo/controller/api/ApiController.java
@@ -1,14 +1,21 @@
 package com.example.demo.controller.api;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 @RestController
 public class ApiController {
 
+  private Gson gson = new Gson();
+
   @PostMapping("/message/{message}")
-  public String message(@PathVariable("message") String message) {
+  public String message(@PathVariable("message") String message, HttpServletRequest req) {
+    System.out.println("req info: " + gson.toJson(req));
     return message;
   }
 }
